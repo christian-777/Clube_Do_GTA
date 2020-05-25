a157fc457ceb34f714085d23f23a0d528a2d33e0
christian-777 <christian.c.g.f777@hotmail.com>
2020-05-25 16:19:35 -0300

    modal

---
+++ js/funcoes.js
@@ -0,0 +1,9 @@
+function erro()
+{
+	<div class="alert alert-warning alert-dismissible fade show" role="alert">
+		<strong>E-mail já Cadastrado!</strong> 
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		</button>
+	</div>
+}
