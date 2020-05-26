ddd736324d9360691ab3a5f9ae73d28d9a9ab7fe
christian-777 <christian.c.g.f777@hotmail.com>
2020-05-26 19:40:40 -0300

    bootstra e css

--- home.php
+++ home.php
@@ -6,9 +6,12 @@
 		<title>Clube Do GTA</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/login.css">
+		
 	</head>
 	<body>
-		
+		<?php
+			include "inc/cabecalho.inc";
+		?>
 		<script src="js/jquery-3.2.1.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script src="js/validaform.min.js"></script>
--- js/funcoes.js
+++
@@ -1,9 +0,0 @@
-function erro()
-{
-	<div class="alert alert-warning alert-dismissible fade show" role="alert">
-		<strong>E-mail já Cadastrado!</strong> 
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			<span aria-hidden="true">&times;</span>
-		</button>
-	</div>
-}
